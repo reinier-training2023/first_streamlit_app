40222050d261774d6710e7fe6b9c9d4a6f3a6be4
reinier-training2023 <126463432+reinier-training2023@users.noreply.github.com>
2023-03-03 15:53:16 +0100

    Update streamlit_app.py
    deleted not-normalized json

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,8 +23,7 @@ streamlit.dataframe(fruits_to_show)
 # New Section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 
 # Normalizing values from JSON to show them as columns 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
