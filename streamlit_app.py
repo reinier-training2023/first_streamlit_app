7b09eea0f3d8fadba24320c6665a98faa34b0847
reinier-training2023 <126463432+reinier-training2023@users.noreply.github.com>
2023-03-03 15:51:36 +0100

    Update streamlit_app.py
    added normalizer for fruityvice JSON data

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,3 +25,8 @@ streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+# Normalizing values from JSON to show them as columns 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# Show the data from fruityvice as a table
+streamlit.dataframe(fruityvice_normalized)
